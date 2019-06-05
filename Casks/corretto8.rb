35699b97f1319df4009ebd7dcda71fdbd403666c
Taras <9948629+Trane9991@users.noreply.github.com>
2019-06-05 13:42:28 +0300

    Add cask to install amazon corretto 8

---
+++ Casks/corretto8.rb
@@ -0,0 +1,14 @@
+cask 'corretto8' do
+  version '8.212.04.2'
+  sha256 'bed8294fe99c1f0425fb3fc26681b08d90482f2845cc3e13a1eb698b4b5d1a22'
+
+  # d3pxv6yz143wms.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d3pxv6yz143wms.cloudfront.net/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
+  appcast "https://docs.aws.amazon.com/en_us/corretto/latest/corretto-#{version.major}-ug/corretto-#{version.major}-ug.rss"
+  name 'Amazon Corretto 8'
+  homepage 'https://aws.amazon.com/corretto/'
+
+  pkg "amazon-corretto-#{version}-macosx-x64.pkg"
+
+  uninstall pkgutil: "com.amazon.corretto.#{version.major}"
+end
